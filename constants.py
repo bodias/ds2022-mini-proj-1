38ec02f746c36275fe42a2586081273b8ff1db05
Amey Darekar <darekar.amey@gmail.com>
2022-03-24 14:09:09 +0200

    [INIT] .gitignore for pycache
    [Update] Added constants for message types and process-timeout

--- constants.py
+++ constants.py
@@ -1,8 +1,15 @@
-
+timeout_lower = 5
 
 process_states = {
 	'want': "WANTED",
 	'held': "held",
 	'do_not_want': "DO-NOT-WANT",
 
+}
+
+
+message_type = {
+	"req": "request",
+	"rel": "release",
+	"ack": "acknowledge"
 }
