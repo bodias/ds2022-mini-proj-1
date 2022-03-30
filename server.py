4c42a0b39db6dc22633cde002cab2f1ba49c9242
Amey Darekar <darekar.amey@gmail.com>
2022-03-30 20:09:22 +0300

    [Update] Added call propogation method for CS release in ProcessService

--- server.py
+++ server.py
@@ -39,19 +39,45 @@ class ProcessService(rpyc.Service):
 	def exposed_cs_request_callback(self, external_timestamp):
 		"""
 			Check Process Monotonic clock and formulate response for Calling ClientService
+
+
+		 	1. receiver is not accessing the resource and doesn't want to access it
+				OUTCOME: Send OK to back to sender
+
+			2. Receiver already has access to the resource
+				OUTCOME: Don't reply, queue the request TODO: Keep?
+
+			3. Receiver is on the waiting list. Compare timestamps, lowest wins.
+				OUTCOME: 
+					If OWN timestamp > request Timestamp (current process loses), then send back OK
+					Otherwise receiver queues incoming request and send nothing back TODO: keep?
 		"""
 		if self.process.state == "do_not_want":
 			return True
-		elif self.process.getMonotonic() > external_timestamp:
-			return True
-		else:
+		elif self.process.state == "held":
 			return False
+		elif self.process.state == "want":
+			if self.process.get_timestamp() > external_timestamp:
+				return True
+		return False
 
 	def request_CS(self):
 		"""
 			Propagate cs_request from ProcessThread instance to ClientService instance
 		"""
-		return self._conn.root.request_access(self.process.getMonotonic())
+		return self._conn.root.request_access(self.process.get_timestamp())
+
+	def access_CS(self):
+		"""
+			Propagate cs_access call from ProcessThread instance to ClientService instance
+		"""
+		return self._conn.root.hold_CS(self.process.id)
+
+	def exposed_release_CS(self):
+		"""
+			Propagate CS RELEASE call from ClientService instance to ProcessThread instance
+		"""
+		return self.process.release_CS()
 
 	def on_disconnect(self,conn):  
 		# print("disconnected on {}\n".format(date_time))
